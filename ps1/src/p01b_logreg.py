93a7307826f727c7387460af001a2928448b9a53
wangzhoujie <jayzhouwang@gmail.com>
2021-01-15 19:16:20 +0800

    finish ps1 1b

--- ps1/src/p01b_logreg.py
+++ ps1/src/p01b_logreg.py
@@ -15,9 +15,19 @@ def main(train_path, eval_path, pred_path):
     x_train, y_train = util.load_dataset(train_path, add_intercept=True)
 
     # *** START CODE HERE ***
+
     # Train a logistic regression classifier
-    # Plot decision boundary on top of validation set set
+    model = LogisticRegression()
+    model.fit(x_train, y_train)
+
+    # Plot decision boundary on top of validation set
+    x_valid, y_valid = util.load_dataset(eval_path, add_intercept=True)
+    util.plot(x_valid, y_valid, model.theta, '{}.png'.format(pred_path[:-4]))
+
     # Use np.savetxt to save predictions on eval set to pred_path
+    y_pred = model.predict(x_valid)
+    np.savetxt(pred_path, y_pred)
+
     # *** END CODE HERE ***
 
 
@@ -38,6 +48,30 @@ class LogisticRegression(LinearModel):
             y: Training example labels. Shape (m,).
         """
         # *** START CODE HERE ***
+
+        # init theta
+        m, n = x.shape
+        # print(m, n)
+        self.theta = np.zeros(n)
+
+        # Newton's method
+        while True:
+            # save old theta
+            theta_old = np.copy(self.theta)
+
+            # compute Hessian Matrix
+            g = 1/(1 + np.exp(-x.dot(self.theta)))
+            hessian = 1 / m * np.dot(g, (1-g).T) * np.dot(x.T, x)
+
+            # compute nabla
+            nabla = 1 / m * np.dot(x.T, g - y)
+
+            # update theta
+            self.theta -= np.linalg.inv(hessian).dot(nabla)
+
+            if np.linalg.norm(self.theta - theta_old, ord=1) < self.eps:
+                break
+
         # *** END CODE HERE ***
 
     def predict(self, x):
@@ -50,4 +84,5 @@ class LogisticRegression(LinearModel):
             Outputs of shape (m,).
         """
         # *** START CODE HERE ***
+        return 1/(1 + np.exp(-x.dot(self.theta)))
         # *** END CODE HERE ***
